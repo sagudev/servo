843f9061b86cbef9400cbfd10e1c3c7b243c32b9
sagudev <16504129+sagudev@users.noreply.github.com>
2023-02-17 09:56:40 +0100

    Update `dom_struct` to edition 2018

--- components/dom_struct/lib.rs
+++ components/dom_struct/lib.rs
@@ -2,13 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-extern crate proc_macro;
-#[macro_use]
-extern crate quote;
-extern crate syn;
-
 use proc_macro::TokenStream;
 use syn::*;
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn dom_struct(args: TokenStream, input: TokenStream) -> TokenStream {
