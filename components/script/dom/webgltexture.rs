35efdf14c6277924be7ba872339007030731d99e
sagudev <16504129+sagudev@users.noreply.github.com>
2023-06-27 16:21:53 +0200

    Port canvas_traits to no_trace

--- components/script/dom/webgltexture.rs
+++ components/script/dom/webgltexture.rs
@@ -48,6 +48,7 @@ jsmanaged_array!(MAX_LEVEL_COUNT * MAX_FACE_COUNT);
 #[dom_struct]
 pub struct WebGLTexture {
     webgl_object: WebGLObject,
+    #[no_trace]
     id: WebGLTextureId,
     /// The target to which this texture was bound the first time
     target: Cell<Option<u32>>,
@@ -545,7 +546,9 @@ pub struct ImageInfo {
     width: u32,
     height: u32,
     depth: u32,
+    #[no_trace]
     internal_format: TexFormat,
+    #[no_trace]
     data_type: Option<TexDataType>,
 }
 
@@ -594,6 +597,7 @@ pub enum TexCompressionValidation {
 
 #[derive(Clone, Copy, Debug, JSTraceable, MallocSizeOf)]
 pub struct TexCompression {
+    #[no_trace]
     pub format: TexFormat,
     pub bytes_per_block: u8,
     pub block_width: u8,
