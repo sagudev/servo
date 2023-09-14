fde837300be13103c0347fc6c01f9a6a8b4759ef
sagudev <16504129+sagudev@users.noreply.github.com>
2023-09-14 16:52:58 +0200

    Update webgpu cts

--- tests/wpt/webgpu/tests/webgpu/common/internal/version.js
+++ tests/wpt/webgpu/tests/webgpu/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '480edec387e8cd5bf5934680050c59a3f7a01438';
+export const version = 'ac6a70ebad24d65cc2f31bfa0a32cc384c93d2aa';
